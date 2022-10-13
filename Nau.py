e9e31b7c381e52bde707069900e0df486a4f0d49
SOC <SOC@imontilivi.cat>
2022-10-13 20:55:01 +0200

    mispelling

--- Nau.py
+++ Nau.py
@@ -14,7 +14,7 @@ def main():
 
     sortir=False
     while not sortir:
-        op = input('Entra una opció')
+        op = input('Entra una opció: ')
         if op=='d':
             #sumar 1 a la variable posX
             posX+=1
